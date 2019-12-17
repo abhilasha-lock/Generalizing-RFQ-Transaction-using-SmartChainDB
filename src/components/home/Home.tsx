4b860366d40967cc6671b7e3b0435bc11dcab76d
Abhilasha Lokannavar <abhilasha.lock@gmail.com>
2019-12-17 17:19:01 -0500

    Added code to Home.tsx

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -23,7 +23,8 @@ import Dropdown from 'react-dropdown';
 import 'react-dropdown/style.css'
 import { callbackify } from 'util';
 import { promised } from 'q';
-
+import * as driver from 'bigchaindb-driver';
+let conn1;
 
 
 
@@ -33,6 +34,15 @@ const conn = new Connection({
   endpoint: "http://localhost:5820"
 });
 const dbName = 'RequestDB'
+
+const map_capabilities = {
+  "Picking": ["Accuracy", "Payload", "ItemSize"],
+  "Inserting": ["Accuracy", "Repeatability"],
+  "Moving": ["Repeatability", "Accuracy"],
+  "Placing": ["Accuracy", "ItemSize"],
+  "TransportFeeding": ["Accuracy", "Speed_x", "Speed_y", "Speed_z"],
+  "Transporting": ["Payload", "ItemSize"]
+}
 
 
 
@@ -94,7 +104,9 @@ type HomeState = {
   form: RFxForm;
   displayFields: boolean;
   queryFormFields: string[];
-
+  options: string[],
+  capabilityOption: string[],
+  capability: string[]
 };
 
 
@@ -117,7 +129,9 @@ class Home extends Component<HomeProps, HomeState> {
     displayFields: false,
     queryFormFields: [],
     form: {},
-    options: []
+    options: [],
+    capabilityOption: [],
+    capability: []
   };
 
 
@@ -165,11 +179,24 @@ class Home extends Component<HomeProps, HomeState> {
   render() {
     const { classes } = this.props;
 
+    let dropdown;
+    console.log("Capabilities");
+    console.log(this.state.validFormValues);
+    if (this.state.validFormValues.option) {
+
+      dropdown = < Dropdown
+        name="Capability"
+        options={this.state.capabilityOption}
+        placeholder={this.state.validFormValues.capabilityOption}
+        onChange={this.getCapabilityFields.bind(this)}
+      />;
+    }
+
     return (
       <main className={classes.layout} >
         <Paper className={classes.paper}>
           <Typography component="h1" variant="h4" align="center">
-            Request for Quote
+            Request for X
           </Typography>
 
           {this.state.loadingForm && (
@@ -182,7 +209,8 @@ class Home extends Component<HomeProps, HomeState> {
                 placeholder={this.state.validFormValues.option}
                 onChange={this.getFields.bind(this)}
               />
-              {console.log(this.state.formFields)}
+              {dropdown}
+              {console.log(this.state)}
               {
                 this.state.formFields.map(field => (
                   < Grid item xs={12} sm={6} key={_kebabCase(field)} >
@@ -191,9 +219,20 @@ class Home extends Component<HomeProps, HomeState> {
                       name={_kebabCase(field)}
                       label={field}
                       onChange={e => this.onFormFieldUpdated(field, e)}
+                      fullWidth
                     />
                   </Grid>
                 ))}
+
+
+              {/* {
+                 
+                 < Dropdown
+                options={this.state.capabilityOption}
+                placeholder={this.state.validFormValues.capabilityOption}
+                onChange={this.getCapabilityFields.bind(this)}
+              />
+              } */}
 
               <div className={classes.btnContainer}>
                 <div className={classes.btnFiller} />
@@ -252,25 +291,125 @@ class Home extends Component<HomeProps, HomeState> {
         formFields: data,
         validFormValues: {
           ...this.state.validFormValues,
-          [_kebabCase("option")]: props.value
+          [_kebabCase("option")]: props.value,
+          "capabilityOption": "Capability"
         },
         form: {
           ...this.state.form,
-          [_kebabCase("option")]: props.value
+          [_kebabCase("option")]: props.value,
+          "capabilityOption": "Capability"
         },
+        capability: ["capability"]
       });
-
       // console.log(data);
       // console.log(this.state.formFields)
+      // const capabilityQuery = `PREFIX cm: <http://resourcedescription.tut.fi/ontology/capabilityModel#>
+      //     PREFIX rm: <http://resourcedescription.tut.fi/ontology/resourceModel#>
+      //     PREFIX re: <http://www.co-ode.org/ontologies/ont.owl#>
 
+      //     SELECT ?g WHERE { ?s rm:hasCapability ?g}`;
+
+      const capabilityQuery = `PREFIX cm: <http://resourcedescription.tut.fi/ontology/capabilityModel#>
+        PREFIX rm: <http://resourcedescription.tut.fi/ontology/resourceModel#>
+        PREFIX re: <http://www.co-ode.org/ontologies/ont.owl#>
+
+        SELECT ?t WHERE { 
+          ?s rm:hasCapability ?g .
+          ?g rdf:type ?t .
+        }`;
+
+      console.log(capabilityQuery);
+      query.execute(conn, dbName, capabilityQuery).then(resp => {
+        if (!resp.ok) {
+
+          throw new Error(
+            `Something went wrong. Received response: ${resp.status} ${resp.statusText}`
+          );
+          return;
+        }
+
+        let set = new Set<string>()
+        console.log("Query results");
+        console.log(resp.body);
+        console.log(resp.body.results.bindings);
+
+
+        let res1 = [];
+        for (var field in resp.body.results.bindings) {
+          if (res1.indexOf(resp.body.results.bindings[field].t.value.split('#')[1]) == -1 && (resp.body.results.bindings[field].t.value.split('#')[1]) != "NamedIndividual")
+            res1.push(resp.body.results.bindings[field].t.value.split('#')[1]);
+        }
+
+        for (var field in res1) { this.state.capabilityOption.push(res1[field]); }
+
+        //const opt = ["1", "2", "3"];
+
+        //for (var field in opt) { this.state.capabilityOption.push(opt[field]); }
+
+        // console.log(data);
+        console.log(this.state.capabilityOption)
+      });
     });
-
 
 
   }
 
+  async getCapabilityFields(props: any) {
+    console.log(props.value);
+    console.log(props);
+
+    const capabilityFieldsQuery = `
+                      `;
+
+    //console.log(query_RFx);
+    /*  query.execute(conn, dbName, capabilityFieldsQuery).then(res => {
+       if (!res.ok) {
+ 
+         throw new Error(
+           `Something went wrong. Received response: ${res.status} ${res.statusText}`
+         );
+         return;
+       }
+ 
+       let set = new Set<string>()
+       console.log("Query results");
+       console.log(res.body);
+       console.log(res.body.results.bindings);
+       console.log(res.body.results.bindings.length);
+       for (var i = 0; i < res.body.results.bindings.length; ++i) {
+         set.add(res.body.results.bindings[i].g.value);
+       } */
+
+    //let data: string[] = Array.from(set.values());
+
+    const capdata = ["size", "weight", "height"];
+
+    var fields = this.state.formFields;
+    fields = fields.concat(map_capabilities[props.value]);
+
+    this.setState({
+      displayFields: true,
+      formFields: fields,
+      validFormValues: {
+        ...this.state.validFormValues,
+        "capabilityOption": props.value
+      },
+      form: {
+        ...this.state.form,
+        "capabilityOption": props.value
+      }
+
+    });
+    console.log(this.state.form.capabilityOption);
+    // console.log(data);
+    // console.log(this.state.formFields)
+
+    // });
+
+  }
+
   submitForm() {
-    this.sendRequestForQuote(this.state.form);
+    this.postTransaction();
   }
 
   onFormFieldUpdated(field: string, event: React.ChangeEvent<HTMLElement>) {
@@ -304,7 +443,7 @@ class Home extends Component<HomeProps, HomeState> {
         );
         return;
       }
-      console.log(res.body.results.bindings[0].s.value);
+      //console.log(res.body.results.bindings[0].s.value);
       // res.body.results = res.body.results.map(field => field.s.value);
       this.setState({ loadingForm: false });
       let res1 = [];
@@ -318,6 +457,7 @@ class Home extends Component<HomeProps, HomeState> {
       console.log(res1);
       // this.setState({ formFields: res1 });
       return res.body.results;
+
     });
 
     return;
@@ -340,7 +480,7 @@ class Home extends Component<HomeProps, HomeState> {
         );
         return;
       }
-      // res.body.results = res.body.results.map(field => field.s.value);
+      res.body.results = res.body.results.map(field => field.s.value);
       this.setState({ loadingForm: false });
       let res1 = [];
       for (var field in res.body.results.bindings) {
@@ -355,6 +495,55 @@ class Home extends Component<HomeProps, HomeState> {
 
     return;
   }
+
+
+  // Post transaction to BigchainDB
+  private async postTransaction() {
+    if (!conn1) {
+      conn1 = new driver.Connection("http://192.168.33.10:9984/api/v1/");
+    }
+
+    const aliceKeypair = new driver.Ed25519Keypair()
+
+    const condition = driver.Transaction.makeEd25519Condition(aliceKeypair.publicKey, true);
+
+    const output = driver.Transaction.makeOutput(condition);
+    output.public_keys = [aliceKeypair.publicKey];
+
+    // define asset and metadata
+    const asset = this.state.form;
+
+    const metadata = {
+      'random_data': Math.random(0, 1)
+    };
+
+    const transaction = driver.Transaction.makeCreateTransaction(
+      asset,
+      metadata,
+      [output],
+      aliceKeypair.publicKey
+    );
+
+    const txSigned = driver.Transaction.signTransaction(transaction, aliceKeypair.privateKey);
+
+    try {
+      let tx = await conn1.postTransaction(txSigned)
+      alert("transaction hash: " + tx.id)
+      return tx
+    } catch (error) {
+      console.error(error);
+      return false
+    }
+  }
+
+
+
+
+
+
+
+
+
 }
 export default withStyles(styles)(Home);
 
